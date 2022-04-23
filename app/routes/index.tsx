31391cc04f77b4598b3bd335ccaa3b6b077ac543
Spencer <other.spencersolberg@outlook.com>
2022-04-23 15:28:01 -0400

    Simplified CSS, fixed cut-off 'g'

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -20,51 +20,51 @@ export default function Index() {
 	return (
 		<>
 			<header className="text-2xl lg:text-4xl">
-				<h2 className="fixed top-0 p-4 glass">Home</h2>
-				<h2 className="fixed top-0 p-4 right-0 text-right glass">
+				<h2 className="fixed top-0 p-4 text-black/50">Home</h2>
+				<h2 className="fixed top-0 p-4 right-0 text-right text-black/50">
 					{visitors} visitor{visitors == 1 ? "" : "s"}
 				</h2>
 			</header>
 			<main className="text-white">
 				<div id="name" className="flex mx-auto">
 					<section className="mx-auto flex flex-col h-screen justify-center leading-[5rem] text-7xl sm:leading-[8rem] sm:text-8xl lg:leading-[11.5rem] lg:text-10xl">
-						<h1 className="glass">Spencer</h1>
-						<h1 className="glass pb-20">Solberg</h1>
+						<h1 className="text-black/50">Spencer</h1>
+						<h1 className="text-black/50 pb-20">Solberg</h1>
 					</section>
 				</div>
 			</main>
 			<footer className="text-2xl lg:text-4xl">
 				<nav className="flex flex-col p-4 fixed bottom-0">
 					<a
-						className="glass hover:text-[#d83bd2] hover:italic"
+						className="text-black/50 hover:text-[#d83bd2] hover:italic"
 						href="mailto:solberg@duck.com"
 						target="_blank"
 					>
 						Contact
 					</a>
 					<a
-						className="glass hover:text-[#6bd968] hover:bg- hover:italic"
+						className="text-black/50 hover:text-[#6bd968] hover:bg- hover:italic"
 						href="https://github.com/spencersolberg"
 						target="_blank"
 					>
 						GitHub
 					</a>
 					<a
-						className="glass hover:text-[#3992ff] hover:bg- hover:italic"
+						className="text-black/50 hover:text-[#3992ff] hover:bg- hover:italic"
 						href="https://www.twitter.com/spencersolberg_"
 						target="_blank"
 					>
 						Twitter
 					</a>
 					<a
-						className="glass hover:text-[#f44250] hover:bg- hover:italic"
+						className="text-black/50 hover:text-[#f44250] hover:bg- hover:italic"
 						href="https://www.instagram.com/spencersolberg"
 						target="_blank"
 					>
 						Instagram
 					</a>
 				</nav>
-				<p className="glass fixed right-0 bottom-0 p-4">
+				<p className="text-black/50 fixed right-0 bottom-0 p-4">
 					Copyright &copy;{" "}
 					<span className="tracking-tight">2022</span>
 				</p>
